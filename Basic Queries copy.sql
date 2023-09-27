0eeecd30e31a8186bfe5b6840d6f9d0467fbc446
noeldelgadom <noeldelgadom@gmail.com>
2023-09-27 10:11:26 -0600

    deleted the copy of basic queries file

--- Basic Queries copy.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM pdf2csv.issues;
-SELECT COUNT(*) FROM pdf2csv.issues where fileSign = '54766fa5';
-SELECT COUNT(*) FROM pdf2csv.cases where fileSign = '54766fa5';
-SELECT * FROM pdf2csv.issues where fileSign = '54766fa5' LIMIT 5;
-SELECT * FROM pdf2csv.cases where fileSign = '54766fa5' LIMIT 5;
-SET SQL_SAFE_UPDATES = 0;
-DELETE FROM pdf2csv.issues where fileSign = '54766fa5';
--- Basic Queries.sql
+++ Basic Queries.sql
@@ -1,5 +1,7 @@
 SELECT * FROM pdf2csv.issues;
-SELECT * FROM pdf2csv.issues where fileSign = 'ebdbda86';
-SELECT * FROM pdf2csv.cases where fileSign = 'ebdbda86';
+SELECT COUNT(*) FROM pdf2csv.issues where fileSign = '54766fa5';
+SELECT COUNT(*) FROM pdf2csv.cases where fileSign = '54766fa5';
+SELECT * FROM pdf2csv.issues where fileSign = '54766fa5' LIMIT 5;
+SELECT * FROM pdf2csv.cases where fileSign = '54766fa5' LIMIT 5;
 SET SQL_SAFE_UPDATES = 0;
-DELETE FROM pdf2csv.issues where fileSign = 'ebdbda86';
+DELETE FROM pdf2csv.issues where fileSign = '54766fa5';
